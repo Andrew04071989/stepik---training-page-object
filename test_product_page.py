a22acf19028f51be1341a5385519e5ab228d6491
Andrew04071989 <kozhuzha@gmail.com>
2020-06-11 19:19:51 +0300

    Refactor class MainPage
    - Move MainPage methods to BasePage;
    - Rewrite tests in test_main_page.py
    according to changes above;
    - Add new tests to test_product_page.py
    based on changes above.

--- test_product_page.py
+++ test_product_page.py
@@ -10,44 +10,44 @@ from selenium.common.exceptions import NoAlertPresentException
 # link_1 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
-# def solve_quiz_and_get_code(browser):
-#     alert = browser.switch_to.alert
-#     x = alert.text.split(" ")[2]
-#     answer = str(math.log(abs((12 * math.sin(float(x))))))
-#     alert.send_keys(answer)
-#     alert.accept()
-#     try:
-#         alert = browser.switch_to.alert
-#         alert_text = alert.text
-#         print(f"Your code: {alert_text}")
-#         alert.accept()
-#     except NoAlertPresentException:
-#         print("No second alert presented")
-#
-#
-# @pytest.mark.parametrize(
-#     'link', [
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#         pytest.param(
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-#             marks=pytest.mark.xfail
-#         ),
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
-#     ]
-# )
-# def test_guest_can_add_product_to_basket(browser, link):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_basket()
-#     solve_quiz_and_get_code(browser)
-#     page.should_be_add_to_basket()
+def solve_quiz_and_get_code(browser):
+    alert = browser.switch_to.alert
+    x = alert.text.split(" ")[2]
+    answer = str(math.log(abs((12 * math.sin(float(x))))))
+    alert.send_keys(answer)
+    alert.accept()
+    try:
+        alert = browser.switch_to.alert
+        alert_text = alert.text
+        print(f"Your code: {alert_text}")
+        alert.accept()
+    except NoAlertPresentException:
+        print("No second alert presented")
+
+
+@pytest.mark.parametrize(
+    'link', [
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+        pytest.param(
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+            marks=pytest.mark.xfail
+        ),
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+    ]
+)
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    solve_quiz_and_get_code(browser)
+    page.should_be_add_to_basket()
 
 
 @pytest.mark.xfail
@@ -77,6 +77,22 @@ def test_message_disappeared_after_adding_to_basket(browser):
     page.add_to_basket()
     page.should_be_disappear()
 
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/" \
+           "catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/" \
+           "catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
 
 
 
